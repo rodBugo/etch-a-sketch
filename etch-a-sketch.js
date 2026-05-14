c35964f899778e44a42786bac11ce998a5f22f2b
Julio Rodriguez-Zambrano <228585521+rodBugo@users.noreply.github.com>
2026-05-14 15:18:44 -0600

    Add logic to change grid size and button usage
    Add function "createGrid" to create and resize the grid
    Function takes user input for size, max size = 100 and default size = 16
    Add new event listener for button functionality
    Button asks for user input and resizes grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,37 @@
 const container = document.getElementById("main-container");
+const button = document.getElementById("resize-button");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+function createGrid(size) {
+    container.innerHTML = "";
 
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("active");
-    });
+    const squareSize = 960 / size;
 
-    container.appendChild(square);
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+
+        square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseover", () => {
+            square.classList.add("active")
+        });
+
+        container.appendChild(square);
+    }
 }
 
+button.addEventListener("click", () => {
+    let userInput = prompt("Enter number of squares per side (max 100):");
+
+    userInput = Number(userInput);
+
+    if (userInput > 0 && userInput <= 100) {
+        createGrid(userInput);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+});
+
+createGrid(16);
