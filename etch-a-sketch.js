d384534ad805d57a777098c92b47679f0a1040fb
Julio Rodriguez-Zambrano <228585521+rodBugo@users.noreply.github.com>
2026-05-14 14:53:35 -0600

    Add event listener to change grid color
    Grid color changes when mouse enters the grid to black

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,4 +4,11 @@ for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
+
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("active");
+    });
+
+    container.appendChild(square);
 }
+
